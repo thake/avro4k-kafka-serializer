eea1bd6b5d09fe723d0edecbc6f6579d76eafa8d
Thorsten Hake <thorsten.hake@dew-stahl.com>
2020-04-06 08:52:22 +0200

    Changed classloader lookup to support context classloaders.
    Thus the problem mentioned in https://github.com/spring-projects/spring-boot/issues/14622 should not occur.

--- src/main/kotlin/com/github/thake/kafka/avro4k/serializer/AbstractKafkaAvro4kDeserializer.kt
+++ src/main/kotlin/com/github/thake/kafka/avro4k/serializer/AbstractKafkaAvro4kDeserializer.kt
@@ -13,41 +13,58 @@ import org.apache.kafka.common.errors.SerializationException
 import org.reflections.Reflections
 import org.reflections.scanners.SubTypesScanner
 import org.reflections.scanners.TypeAnnotationsScanner
+import org.reflections.util.ClasspathHelper
 import org.reflections.util.ConfigurationBuilder
 import java.io.IOException
 import java.nio.ByteBuffer
 
 @ImplicitReflectionSerializer
 abstract class AbstractKafkaAvro4kDeserializer : AbstractKafkaAvroSerDe() {
+    private var specificLookupForClassLoader: MutableMap<ClassLoader, Lookup> = mutableMapOf()
     private var recordPackages: List<String>? = null
-    //A map of alternative names for types that are annotated with Avro annotations
 
-    private val recordNameToType: Map<String, Class<*>> by lazy {
-        val currentPackages = recordPackages ?: throw IllegalStateException(
-            "Couldn't find record by schema name. " +
-                    "Tried to loookup types with @AvroName, @AvroNamespace and @AvroAliases annotations but either config parameter" +
-                    "'${KafkaAvro4kDeserializerConfig.RECORD_PACKAGES}' has not been set, or config has not been called."
-        )
-        if (currentPackages.isNotEmpty()) {
-            val configBuilder = ConfigurationBuilder().addScanners(TypeAnnotationsScanner(), SubTypesScanner())
-                    .forPackages(*currentPackages.toTypedArray())
+    inner class Lookup(private val classLoader: ClassLoader) {
+        private val specificData = SpecificData(classLoader)
+        private val recordNameToType: Map<String, Class<*>> by lazy {
+            val currentPackages = recordPackages ?: throw IllegalStateException(
+                "Couldn't find record by schema name. " +
+                        "Tried to loookup types with @AvroName, @AvroNamespace and @AvroAliases annotations but either config parameter" +
+                        "'${KafkaAvro4kDeserializerConfig.RECORD_PACKAGES}' has not been set, or config has not been called."
+            )
+            if (currentPackages.isNotEmpty()) {
+                val configBuilder = ConfigurationBuilder().addScanners(TypeAnnotationsScanner(), SubTypesScanner())
+                currentPackages.forEach { configBuilder.addUrls((ClasspathHelper.forPackage(it, classLoader))) }
 
-            val reflection = Reflections(configBuilder)
-            val avroTypes = HashSet<Class<*>>().apply {
-                this.addAll(reflection.getTypesAnnotatedWith(AvroName::class.java, true))
-                this.addAll(reflection.getTypesAnnotatedWith(AvroNamespace::class.java, true))
-                this.addAll(reflection.getTypesAnnotatedWith(AvroAlias::class.java, true))
-                this.addAll(reflection.getTypesAnnotatedWith(AvroAliases::class.java, true))
+                val reflection = Reflections(configBuilder)
+                val avroTypes = HashSet<Class<*>>().apply {
+                    this.addAll(reflection.getTypesAnnotatedWith(AvroName::class.java, true))
+                    this.addAll(reflection.getTypesAnnotatedWith(AvroNamespace::class.java, true))
+                    this.addAll(reflection.getTypesAnnotatedWith(AvroAlias::class.java, true))
+                    this.addAll(reflection.getTypesAnnotatedWith(AvroAliases::class.java, true))
+                }
+                avroTypes.flatMap { type ->
+                    getTypeNames(type).map { Pair(it, type) }
+                }.toMap()
+            } else {
+                emptyMap()
             }
-            avroTypes.flatMap { type ->
-                getTypeNames(type).map { Pair(it, type) }
-            }.toMap()
-        }else{
-            emptyMap()
+        }
+
+        fun lookupType(msgSchema: Schema): Class<*>? {
+            //Use the context classloader to not run into https://github.com/spring-projects/spring-boot/issues/14622 when
+            //using Spring boot devtools
+            var objectClass: Class<*>? = specificData.getClass(msgSchema)
+            if (objectClass == null) {
+                objectClass = recordNameToType[msgSchema.fullName]
+            }
+            return objectClass
         }
     }
 
-    protected fun getTypeNames(type : Class<*>) : List<String> {
+    //A map of alternative names for types that are annotated with Avro annotations
+
+
+    protected fun getTypeNames(type: Class<*>): List<String> {
         val descriptor = type.kotlin.serializer().descriptor
         val naming = RecordNaming(descriptor)
         val aliases = AnnotationExtractor(descriptor.getEntityAnnotations()).aliases()
@@ -105,34 +122,52 @@ abstract class AbstractKafkaAvro4kDeserializer : AbstractKafkaAvroSerDe() {
                 val length = buffer.limit() - 1 - 4
                 val bytes = ByteArray(length)
                 buffer[bytes, 0, length]
-                if (schema.type == Schema.Type.BYTES) {
-                    bytes
-                } else {
-                    val reader = getSerializer(schema)
-                    Avro.default.openInputStream(reader) {
-                        format = AvroFormat.BinaryFormat
-                        writerSchema = schema
-                    }.from(bytes).nextOrThrow()
-                }
+                return deserialize(schema, bytes)
             } catch (re: RuntimeException) {
-                throw SerializationException("Error deserializing Avro message for schema id $id with avro4k",re)
+                throw SerializationException("Error deserializing Avro message for schema id $id with avro4k", re)
             } catch (io: IOException) {
-                throw SerializationException("Error deserializing Avro message for schema id $id with avro4k",io)
+                throw SerializationException("Error deserializing Avro message for schema id $id with avro4k", io)
             } catch (registry: RestClientException) {
-                throw SerializationException("Error retrieving Avro schema for id $id from schema registry.",registry)
+                throw SerializationException("Error retrieving Avro schema for id $id from schema registry.", registry)
             }
         }
     }
 
+    fun deserialize(schema: Schema, bytes: ByteArray) =
+        when (schema.type) {
+            Schema.Type.BYTES -> bytes
+            Schema.Type.ARRAY -> {
+
+            }
+            else -> {
+                val reader = getSerializer(schema)
+                Avro.default.openInputStream(reader) {
+                    format = AvroFormat.BinaryFormat
+                    writerSchema = schema
+                }.from(bytes).nextOrThrow()
+            }
+        }
+
 
     protected open fun getSerializer(msgSchema: Schema): KSerializer<*> {
-        var objectClass = SpecificData.get().getClass(msgSchema)
-        if(objectClass == null){
-           objectClass = recordNameToType[msgSchema.fullName] ?: throw SerializationException("Couldn't find matching class for record type ${msgSchema.fullName}. Full schema: $msgSchema")
+        //First lookup using the context class loader
+        val contextClassLoader = Thread.currentThread().contextClassLoader
+        var objectClass: Class<*>? = null
+        if (contextClassLoader != null) {
+            objectClass = getLookup(contextClassLoader).lookupType(msgSchema)
+        }
+        if (objectClass == null) {
+            //Fallback to classloader of this class
+            objectClass = getLookup(AbstractKafkaAvro4kDeserializer::class.java.classLoader).lookupType(msgSchema)
+                ?: throw SerializationException("Couldn't find matching class for record type ${msgSchema.fullName}. Full schema: $msgSchema")
         }
 
         return objectClass.kotlin.serializer()
     }
+
+    private fun getLookup(classLoader: ClassLoader) =
+        specificLookupForClassLoader.getOrPut(classLoader,
+            { Lookup(classLoader) })
 
 
 }
---
+++ src/test/kotlin/com/github/thake/kafka/avro4k/serializer/ClassloaderTest.kt
@@ -0,0 +1,102 @@
+package com.github.thake.kafka.avro4k.serializer
+
+import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig
+import kotlinx.serialization.ImplicitReflectionSerializer
+import kotlinx.serialization.Serializable
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Test
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.IOException
+import java.util.concurrent.CountDownLatch
+
+@Serializable
+data class SimpleTest(
+    val str: String
+)
+
+@ImplicitReflectionSerializer
+class ClassloaderTest {
+    private val config: Map<String, String> = mapOf(
+        KafkaAvro4kDeserializerConfig.RECORD_PACKAGES to this::class.java.packageName,
+        AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG to "mock://registry"
+    )
+    private val serializer = KafkaAvro4kSerializer(null, config).apply { configure(config, false) }
+    private val deserializer = KafkaAvro4kDeserializer(null, config).apply { configure(config, false) }
+
+    @Test
+    fun deserializeWithDifferentClassloader() {
+        val byteArray = serializer.serialize("A", SimpleTest("AAA"))
+        val newClassLoader = object : ClassLoader() {
+
+            override fun loadClass(name: String): Class<*> {
+                return if (name.contains("com.github.thake.kafka.avro4k.serializer.SimpleTest")) findClass(name)
+                else super.loadClass(name)
+            }
+
+            @Throws(ClassNotFoundException::class)
+            override fun findClass(name: String): Class<*> {
+                val b = loadClassFromFile(name)
+                return defineClass(name, b, 0, b.size, null)
+            }
+
+            private fun loadClassFromFile(fileName: String): ByteArray {
+                val inputStream = javaClass.classLoader
+                    .getResourceAsStream(fileName.replace('.', File.separatorChar) + ".class")
+                val buffer: ByteArray
+                val byteStream = ByteArrayOutputStream()
+                var nextValue = 0
+                try {
+                    while (inputStream.read().also { nextValue = it } != -1) {
+                        byteStream.write(nextValue)
+                    }
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+                buffer = byteStream.toByteArray()
+                return buffer
+            }
+        }
+        val countDown = CountDownLatch(1)
+        var result: Any? = null
+        val testThread = Thread {
+            assertEquals(newClassLoader, Thread.currentThread().contextClassLoader)
+            result = deserializer.deserialize("s", byteArray)
+            countDown.countDown()
+        }
+        testThread.contextClassLoader = newClassLoader
+        testThread.start()
+        countDown.await()
+
+        assertNotNull(result)
+        assertEquals(newClassLoader, result!!.javaClass.classLoader)
+    }
+
+    @Test
+    fun deserializeWithoutContextClassloader() {
+        val byteArray = serializer.serialize("A", SimpleTest("AAA"))
+        val countDown = CountDownLatch(1)
+        var result: Any? = null
+        val testThread = Thread {
+            result = deserializer.deserialize("s", byteArray)
+            countDown.countDown()
+        }
+        testThread.contextClassLoader = null
+        testThread.start()
+        countDown.await()
+
+        assertNotNull(result)
+        assertEquals(ClassloaderTest::class.java.classLoader, result!!.javaClass.classLoader)
+    }
+
+
+    @Test
+    fun deserializeWithNormalClassloader() {
+        val byteArray = serializer.serialize("A", SimpleTest("AAA"))
+        var result = deserializer.deserialize("s", byteArray)
+
+        assertNotNull(result)
+        assertEquals(ClassloaderTest::class.java.classLoader, result!!.javaClass.classLoader)
+    }
+}
